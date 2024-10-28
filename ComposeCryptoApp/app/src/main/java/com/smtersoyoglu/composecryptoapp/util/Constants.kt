39853e5fa2c58361d741654eaaa7f81be97c7878
smter <smtersoyoglu231@gmail.com>
2024-10-28 18:57:21 +0300

    Added Resource sealed class and Constants object with API base URL

---
+++ ComposeCryptoApp/app/src/main/java/com/smtersoyoglu/composecryptoapp/util/Constants.kt
@@ -0,0 +1,10 @@
+package com.smtersoyoglu.composecryptoapp.util
+
+object Constants {
+
+    //https://raw.githubusercontent.com/atilsamancioglu/IA32-CryptoComposeData/main/crypto.json
+    //https://raw.githubusercontent.com/atilsamancioglu/K21-JSONDataSet/master/crypto.json
+
+    const val BASE_URL = "https://raw.githubusercontent.com/"
+
+}
