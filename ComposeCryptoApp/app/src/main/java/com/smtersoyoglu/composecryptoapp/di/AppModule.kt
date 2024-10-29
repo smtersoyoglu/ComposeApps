628437d8abeae61e6f0130eeed2a611e56d47e54
smter <smtersoyoglu231@gmail.com>
2024-10-29 17:10:00 +0300

    feat(di): Hilt dependency injection installed for CryptoAPI and CryptoRepository

---
+++ ComposeCryptoApp/app/src/main/java/com/smtersoyoglu/composecryptoapp/di/AppModule.kt
@@ -0,0 +1,35 @@
+package com.smtersoyoglu.composecryptoapp.di
+
+import com.smtersoyoglu.composecryptoapp.repo.CryptoRepository
+import com.smtersoyoglu.composecryptoapp.service.CryptoAPI
+import com.smtersoyoglu.composecryptoapp.util.Constants.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+@Module
+ @InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideCryptoRepository(
+        api: CryptoAPI
+    ) = CryptoRepository(api)
+
+
+    @Singleton
+    @Provides
+    fun provideCryptoApi() : CryptoAPI {
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
+            .create(CryptoAPI::class.java)
+    }
+
+}
---
+++ ComposeCryptoApp/app/src/main/java/com/smtersoyoglu/composecryptoapp/di/HiltApplication.kt
@@ -0,0 +1,9 @@
+package com.smtersoyoglu.composecryptoapp.di
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class HiltApplication : Application() {
+
+}
