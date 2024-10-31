47b7982a8243d383746997fe50b74056ff09ba4c
smter <smtersoyoglu231@gmail.com>
2024-10-31 18:18:15 +0300

    Implemented enhanced data handling and UI in CryptoDetailScreen with optimal recomposition

--- ComposeCryptoApp/app/src/main/java/com/smtersoyoglu/composecryptoapp/view/CryptoDetailScreen.kt
+++ ComposeCryptoApp/app/src/main/java/com/smtersoyoglu/composecryptoapp/view/CryptoDetailScreen.kt
@@ -1,9 +1,117 @@
 package com.smtersoyoglu.composecryptoapp.view
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.produceState
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import coil.compose.AsyncImage
+import com.smtersoyoglu.composecryptoapp.model.Crypto
+import com.smtersoyoglu.composecryptoapp.util.Resource
+import com.smtersoyoglu.composecryptoapp.viewmodel.CryptoDetailViewModel
 
 @Composable
-fun CryptoDetailScreen(navController: NavController, id: String, price: String) {
-    
+fun CryptoDetailScreen(
+    id: String,
+    price: String,
+    navController: NavController,
+    viewModel: CryptoDetailViewModel = hiltViewModel()
+) {
+
+    /*
+    Bu yöntemde sürekli recompose yapılarak görüntüyü yeniden yüklemeye çalışır ve veriler tekrar tekrar çekilmeye çalışır. (devamlı istek atıyor)
+    //Step 1 -> Wrong (yanlis yontem)
+       val scope = rememberCoroutineScope()
+       //Stateless
+       //var cryptoItem : Resource<Crypto> = Resource.Loading()
+       //Stateful
+       var cryptoItem by remember { mutableStateOf<Resource<Crypto>>(Resource.Loading())}
+         scope.launch {
+             cryptoItem = viewModel.getCrypto(id)
+             println(cryptoItem.data)
+         }
+      */
+
+
+    /*S tep 2 -> Better
+    //Stateless
+       //var cryptoItem : Resource<Crypto> = Resource.Loading()
+       //Stateful
+       var cryptoItem by remember { mutableStateOf<Resource<Crypto>>(Resource.Loading())}
+         LaunchedEffect(key1 = Unit) {
+             cryptoItem = viewModel.getCrypto(id)
+             //println(cryptoItem.data)
+         }
+         */
+
+    //Step 3 -> Best
+
+    val cryptoItem by produceState<Resource<Crypto>>(initialValue = Resource.Loading()) {
+        value = viewModel.getCrypto(id)
+    }
+
+    Box(modifier = Modifier
+        .fillMaxSize()
+        .background(MaterialTheme.colorScheme.secondary),
+        contentAlignment = Alignment.Center,
+    ) {
+
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            when(cryptoItem) {
+                is Resource.Success -> {
+                    val selectedCrypto = cryptoItem.data!![0]
+                    Text(text = selectedCrypto.name,
+                        style = MaterialTheme.typography.headlineMedium,
+                        modifier = Modifier.padding(2.dp),
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colorScheme.primary,
+                        textAlign = TextAlign.Center)
+
+                    AsyncImage(
+                        model = selectedCrypto.logo_url,
+                        contentDescription = selectedCrypto.name,
+                        modifier = Modifier
+                            .padding(16.dp)
+                            .size(200.dp, 200.dp)
+                            .clip(CircleShape)
+                            .border(2.dp, Color.Gray, CircleShape)
+                    )
+
+                    Text(text = price,
+                        style = MaterialTheme.typography.headlineSmall,
+                        modifier = Modifier.padding(2.dp),
+                        fontWeight = FontWeight.Bold,
+                        color = MaterialTheme.colorScheme.primaryContainer,
+                        textAlign = TextAlign.Center
+
+                    )
+                }
+
+                is Resource.Error -> {
+                    Text(cryptoItem.message!!)
+                }
+                is Resource.Loading -> {
+                    CircularProgressIndicator(color = MaterialTheme.colorScheme.primary)
+                }
+            }
+        }
+    }
 }
